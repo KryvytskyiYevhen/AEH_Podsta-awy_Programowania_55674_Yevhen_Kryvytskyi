788db3e2299493e9092fdf9aed402feb69a6411d
KryvytskyiYvehen <Kryvytskyiyevhen4@gmail.com>
2024-03-05 10:33:40 +0100

    Zapis do repozytorium Lab2

--- src/pl/pp/mojaDrugaAplikacja.java
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -3,35 +3,20 @@ import java.util.Scanner;
 
 public class mojaDrugaAplikacja {
     public static void main(String[] args) {System.out.println("Witaj PP!");
-        //to je komentarz blokowy
-        Scanner scanner = new Scanner (System.in);
-        /*
-        To jest komentarz blokowy
-        może on mieć wiele linii tak, jak tutaj
-         */
-        System.out.println("Proszę padaj swoje imię:");
-        String forename = scanner.nextLine();
-        System.out.println("Proszę podaj swoje nazwisko:");
-        String surname = scanner.nextLine();
-        scanner.close();
-        System.out.println("Witaj " + forename + " " + surname);
 
-        double x=10;//utworzenie zmiennej typu double i przypisanie jej wartości 10
-        double y=2;
-        //Scanner scanner = new Scanner(System.in);
-        var result = x + y;
-        System.out.println("x + y = " + result);
-        result = x - y;
-        System.out.println("x - y = " + result);
-        result = x * y;
-        System.out.println("x * y = " + result);
-        result = x / y;
-        System.out.println("x / y = " + result);
-        result = x % y;
-        System.out.println("x % y = " + result);
-        System.out.println("Wpisr dwie liczby rozdielone klawiszem Enter:");
-        double first = scanner.nextDouble();//prośba o wartości typu double
-        double second = scanner.nextDouble();
-        System.out.println("x + y = "+ (first + second));
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Proszę padaj swoj wiek w latach:");
+        int wiek= scanner.nextInt();
+        int wiek_w_sekundach = wiek * 365 * 24 * 60;
+        System.out.println("Twój wiek przeliczony na sekundy wunosi:" + wiek_w_sekundach);
+        scanner.close();
+
+        int x = 10;
+        int dwukrottnosc_x = 2 * x;
+        int x_kwadrat = x * x;
+        System.out.println("Wartość x:" + x);
+        System.out.println("Dwukrotność x: " + dwukrottnosc_x);
+        System.out.println("Kwadrat x:" +x_kwadrat);
+
     }
 }
